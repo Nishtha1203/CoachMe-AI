95e72d4f4a26d45c84f04d84a2e65bbf81ee97d7
Nishtha Pabreja <nishthapabreja@Nishthas-MacBook-Pro.local>
2026-04-25 21:29:17 +0530

    Cron setup

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -7,7 +7,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-2.5-flash-lite",
+    model: "gemini-2.5-flash",
 });
 export const generateAIInsights = async (industry) => {
     const prompt = `
